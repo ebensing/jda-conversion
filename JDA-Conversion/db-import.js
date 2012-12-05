eb47993762cc0b28bcf1005941bf09d11d6ef3b8
E.J. Bensing <ebensing@rm-dash-r.com>
2012-12-05 11:59:51 -0500

    added sql connection stuff

--- JDA-Conversion/db-import.js
+++ JDA-Conversion/db-import.js
@@ -1,5 +1,18 @@
+var mysql = require('mysql');
 var fs = require('fs');
 var cnfTxt = fs.readFileSync('../../.my.cnf').toString();
 var password = cnfTxt.substring(cnfTxt.lastIndexOf("=") + 2, cnfTxt.length - 3);
-console.log(password);
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'jedarchi',
+    password: password
+});
+connection.connect();
+connection.query("SELECT * FROM Seeds where verified != 2", function (err, rows, fields) {
+    if(err) {
+        console.log("There be drgns here");
+    } else {
+        console.log("working!!");
+    }
+});
 //@ sourceMappingURL=db-import.js.map
--- JDA-Conversion/db-import.ts
+++ JDA-Conversion/db-import.ts
@@ -1,18 +1,16 @@
 /// <reference path='./node.d.ts' />
 
-//var mysql = require('mysql');
+var mysql = require('mysql');
 var fs = require('fs');
 
 var cnfTxt = fs.readFileSync('../../.my.cnf').toString();
 
 var password = cnfTxt.substring(cnfTxt.lastIndexOf("=")+2, cnfTxt.length - 3);
 
-console.log(password);
-/*
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'jedarchi',
-  password : 'CHANGE THIS',
+  password : password,
 });
 
 connection.connect();
@@ -21,6 +19,6 @@ connection.query("SELECT * FROM Seeds where verified != 2", function (err, rows,
     if (err) {
         console.log("There be drgns here");
     } else {
-
+        console.log("working!!");
     }
-});*/
+});
