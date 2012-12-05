f0d1eae0cb2d9504a4de8f44dfb720b543c9f4ca
E.J. Bensing <ebensing@rm-dash-r.com>
2012-12-05 11:57:56 -0500

    try again

--- JDA-Conversion/db-import.ts
+++ JDA-Conversion/db-import.ts
@@ -5,7 +5,7 @@ var fs = require('fs');
 
 var cnfTxt = fs.readFileSync('../../.my.cnf').toString();
 
-var password = cnfTxt.substring(cnfTxt.lastIndexOf("=")+2, cnfTxt.length - 2);
+var password = cnfTxt.substring(cnfTxt.lastIndexOf("=")+2, cnfTxt.length - 3);
 
 console.log(password);
 /*
