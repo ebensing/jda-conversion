27f7443264843eddde7a742f1ec757322b9b0545
E.J. Bensing <ebensing@rm-dash-r.com>
2012-12-05 16:50:24 -0500

    alpha v1

--- JDA-Conversion/db-import.ts
+++ JDA-Conversion/db-import.ts
@@ -1,6 +1,8 @@
 /// <reference path='./node.d.ts' />
 
 var mysql = require('mysql');
+var async = require('async');
+
 var fs = require('fs');
 
 var cnfTxt = fs.readFileSync('../../.my.cnf').toString();
@@ -16,10 +18,75 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
+var jsonDict = {
+    items : []
+};
+
 connection.query("SELECT * FROM seeds where verified != 2", function (err, rows, fields) {
     if (err) {
         console.log(err);
     } else {
-        console.log("working!!");
+        async.forEach(rows, function (x : any, cb) {
+            var item = {};
+            item['title'] = x.title;
+            item['description'] = x.description;
+            item['uri'] = x.url;
+            item['attribution_uri'] = x.url;
+            item['archive'] = "Reischauer Institute";
+            switch (x.category) {
+                case "website":
+                    item['media_type'] = "website";
+                    item['layer_type'] = "website";
+                    break;
+                case "blog":
+                    item['media_type'] = "website";
+                    item['layer_type'] = "website";
+                    break;
+                case "video":
+                    item['media_type'] = "video";
+                    item['layer_type'] = "video";
+                    break;
+                case "audio":
+                    item['media_type'] = "audio";
+                    item['layer_type'] = "audio";
+                    break;
+                case "photos":
+                    item['media_type'] = "image";
+                    item['layer_type'] = "image";
+                    break;
+                case "maps":
+                    item['media_type'] = "website";
+                    item['layer_type'] = "website";
+                    break;
+                default:
+                    item['media_type'] = "website";
+                    item['layer_type'] = "website";
+            }
+            item['location'] = x.location;
+            item['media_geo_latitude'] = x.lat;
+            item['media_geo_longitude'] = x.lng;
+
+            item['media_date_created'] = x.added;
+            item['media_creator_username'] = x.name;
+            item['id_at_source'] = x.sid;
+
+            item['attributes'] = {
+                frequency: x.frequency,
+                scope: x.scope,
+                language: x.language,
+                creator_email: x.email,
+            };
+
+            item['published'] = 1;
+            var tags = x.tags.split(',');
+            item['tags'] = [];
+            for (var i = 0; i < tags.length; i++) {
+                if (tags[i].trim() != "") {
+                    item['tags'].push(tags[i].trim());
+                }
+            }
+        }, function (err) {
+            fs.writeFileSync('output.json', JSON.stringify(jsonDict));
+        });
     }
 });
