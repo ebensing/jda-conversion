364162282459d37c0eb16669df3475b455aca688
E.J. Bensing <ebensing@rm-dash-r.com>
2012-12-05 11:57:02 -0500

    cut out bad stuff

--- JDA-Conversion/db-import.js
+++ JDA-Conversion/db-import.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
 var cnfTxt = fs.readFileSync('../../.my.cnf').toString();
-var password = cnfTxt.substring(cnfTxt.lastIndexOf("="));
+var password = cnfTxt.substring(cnfTxt.lastIndexOf("=") + 2, cnfTxt.length - 2);
 console.log(password);
 //@ sourceMappingURL=db-import.js.map
--- JDA-Conversion/db-import.ts
+++ JDA-Conversion/db-import.ts
@@ -5,7 +5,7 @@ var fs = require('fs');
 
 var cnfTxt = fs.readFileSync('../../.my.cnf').toString();
 
-var password = cnfTxt.substring(cnfTxt.lastIndexOf("="));
+var password = cnfTxt.substring(cnfTxt.lastIndexOf("=")+2, cnfTxt.length - 2);
 
 console.log(password);
 /*
